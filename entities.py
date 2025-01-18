42ac54431a303251e2d8bff026b7ed9f9f5eecb7
lambda-for-wavelength <147173852+lambda-for-wavelength@users.noreply.github.com>
2025-01-18 14:40:04 -0500

    updated entities

--- entities.py
+++ entities.py
@@ -3,7 +3,24 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
 
-Ground = Entity(model='plane', texture='texture_name', scale=(3,1,1))
-Ground.color = rgb(.61, 117, 38)
-Ground.position = Vec3(0,0,0)
-Ground.rotation = (0,0,0)
+class physical_object:
+    def __init__(self, model, collider , scale, texture, texture_scale):
+        self.model = model
+        self.collider = collider
+        self.scale = scale
+        self.texture = texture
+        self.texture_scale = texture_scale
+        pass
+
+class non_physical_object:
+    def __init__(self, model, scale, texture, texture_scale):
+        self.model = model
+        self.scale = scale
+        self.texture = texture
+        self.texture_scale = texture_scale
+        pass
+
+ground = physical_object(Entity(model = 'plane', collider = 'box', scale = 64, texture = 'grass', texture_scale = (4,4)))
+
+shotgun = non_physical_object(Entity(model='cube', parent=camera))
+shotgun.muzzle_flash = non_physical_object(Entity(parent = shotgun, color = color.yellow, enabled=False))
