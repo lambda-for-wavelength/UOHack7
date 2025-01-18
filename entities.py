637b1ba524b738b168efc5139ca26bfb9b4c07d5
lambda-for-wavelength <147173852+lambda-for-wavelength@users.noreply.github.com>
2025-01-18 15:17:11 -0500

    added the Baseline of the code from ursinae

--- entities.py
+++ entities.py
@@ -3,24 +3,18 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import lit_with_shadows_shader
 
-class physical_object:
-    def __init__(self, model, collider , scale, texture, texture_scale):
-        self.model = model
-        self.collider = collider
-        self.scale = scale
-        self.texture = texture
-        self.texture_scale = texture_scale
-        pass
+class physical_Entity(Entity):
+    def __init_(self):
+        super.__init__(self)
+        self.collider = 'box'
 
-class non_physical_object:
-    def __init__(self, model, scale, texture, texture_scale):
-        self.model = model
-        self.scale = scale
-        self.texture = texture
-        self.texture_scale = texture_scale
-        pass
+class non_physical_Entity(Entity):
+    pass
 
-ground = physical_object(Entity(model = 'plane', collider = 'box', scale = 64, texture = 'grass', texture_scale = (4,4)))
+class living_Entity(Entity):
+    pass
 
-shotgun = non_physical_object(Entity(model='cube', parent=camera))
-shotgun.muzzle_flash = non_physical_object(Entity(parent = shotgun, color = color.yellow, enabled=False))
+#ground = physical_Entity(Entity(model = 'plane', collider = 'box', scale = 64, texture = 'grass', texture_scale = (4,4)))
+
+#shotgun = non_physical_Entity(Entity(model='cube', parent=camera))
+#shotgun.muzzle_flash = non_physical_Entity(Entity(parent = shotgun, color = color.yellow, enabled=False))
